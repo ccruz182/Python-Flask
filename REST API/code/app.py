053250c52a0e40d11552d43144a986a2bf7156e3
Cesar Cruz <cesar.cruz182@gmail.com>
2018-12-11 20:57:52 -0600

    All HTTP Verbs implemented

--- REST API/code/app.py
+++ REST API/code/app.py
@@ -1,13 +1,19 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
 app.secret_key = 'cesar'
 api = Api(app) # Allows to add Resources
 
+jwt = JWT(app, authenticate, identity) # /auth
+
 items = []
 
 class Item(Resource):
+	@jwt_required()
 	def get(self, name):
 		item = filter(lambda item: item['name'] == name, items)
 		if item == []:
@@ -24,6 +30,24 @@ class Item(Resource):
 			return itemC, 201
 		else:
 			return {'message': 'Item already exists'}, 400 
+
+	def delete(self, name):
+		global items
+		items = list(filter(lambda x: x['name'] != name, items))
+		return {'message': 'Item deleted'}
+
+	def put(self, name):
+		item = filter(lambda item: item['name'] == name, items)
+		data = request.get_json()
+		
+		if item == []:
+			itemC = {'name': name, 'price': data['price']}
+			items.append(itemC)			
+		else:
+			item[0].update(data)
+
+		return item
+
 
 class Items(Resource):
 	def get(self):
